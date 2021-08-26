f4a9ebd9b8538e0883ba47f63095bb3bc715ae4e
comrade77 <nimec77@gmail.com>
2021-08-26 17:00:53 +0700

    Проверка с ошибками

--- lib/src/errors/smarteam_error.dart
+++ lib/src/errors/smarteam_error.dart
@@ -1,3 +1,6 @@
+import 'package:meta/meta.dart';
+
+@immutable
 class SmarteamError extends Error {
   SmarteamError(this.message) : super();
 
@@ -5,4 +8,12 @@ class SmarteamError extends Error {
 
   @override
   String toString() => 'SmarteamError: $message';
+
+  @override
+  int get hashCode => message.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) || other is SmarteamError && message == other.message;
+  }
 }
